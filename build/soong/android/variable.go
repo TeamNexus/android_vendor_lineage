5507b695717f8770eeadaf0c632b0c06fbdf0aaa
Lukas0610 <mail@lukasberger.at>
2018-04-28 20:46:43 +0200

    build: soong: remove duplicated variable

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -36,6 +36,5 @@ type ProductVariables struct {
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_samsung_cameraformat_nv21  *bool `json:",omitempty"`
-	Target_shim_libs *string `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
